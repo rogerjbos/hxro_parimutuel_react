29353e9f6f319269759c980a10a65bf876b9a9cb
Thales <81639334+cinojosa0705@users.noreply.github.com>
2023-02-08 13:41:04 -0500

    cleaning up doc-template

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -26,22 +26,14 @@ export const HomeView: FC = ({ }) => {
     <div className="md:hero mx-auto p-4">
       <div className="md:hero-content flex flex-col">
         <h1 className="text-center text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#9945FF] to-[#14F195]">
-          {/* Parimutuel Protocol */}
-          Gm World!
+          Parimutuel Protocol
         </h1>
-        {/* {wallet && <p>SOL Balance: {(balance || 0).toLocaleString()}</p>} */}
-        <div className="text-center" style={{ alignContent: 'center', marginTop: '30px', fontSize: '25px'}}>
-          {/* vvvvvvDELETE THISvvvvvv */}
-          <h1>This is our starter template for our project,<br/>in this tutorial walthrough you will learn how to use:</h1>
-          <h1 style={{marginTop: '10px'}}><code style={{color: 'teal'}}>placePosition()</code></h1>
-          <h1><code style={{color: 'teal'}}>getUserPosition()</code></h1>
-          <h1><code style={{color: 'teal'}}>cancelPosition()</code></h1>
-          <h1 style={{marginTop: '10px'}}>From the Parimutuel Typescript SDK, and make a nice UI our users will love to interact width! <br/>Let's get right into it!</h1>
-          {/* ^^^^^DELETE THIS^^^^^ */}
+        {wallet && <p>SOL Balance: {(balance || 0).toLocaleString()}</p>}
+        <div className="text-center" style={{ alignContent: 'center', marginTop: '30px', fontSize: '25px' }}>
+          {/*Place components bellow*/}
         </div>
       </div>
     </div>
-
   );
 };
 
