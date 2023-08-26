c3bdad7d280270f55233c2cfb9f2ada56e0a8527
Roger J. Bos, CFA <roger.bos@gmail.com>
2023-08-26 13:57:14 -0400

    step8

--- src/components/PlacePositionBox.tsx
+++ src/components/PlacePositionBox.tsx
@@ -1,1 +1,33 @@
-export {}
+
+import { PositionSideEnum } from '@hxronetwork/parimutuelsdk'; import React, { FC, useState } from 'react'; import { useEffect } from 'react';
+
+const PlacePositionBox: FC<{ pubkey: string }> = (props) => {
+    const { pubkey } = props
+
+    const [inputValue, setInputValue] = useState('Enter Amount...');
+    const [amount, setAmount] = useState('0')
+
+    useEffect(() => {
+    }, [pubkey]);
+
+    if (pubkey === 'Loading') {
+        return (
+        
+        Loading...
+        
+        )
+    }
+
+    const handleChange = (event) => {
+        setInputValue(event.target.value);
+        setAmount(event.target.value);
+
+        const handleChange = (event) => {
+            setInputValue(event.target.value);
+            setAmount(event.target.value);
+            if (!event.target.value) {
+                setInputValue('Enter Amount...');
+            }
+        };
+
+        
